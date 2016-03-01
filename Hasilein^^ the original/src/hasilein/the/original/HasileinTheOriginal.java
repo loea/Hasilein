26c809954707ceebd8b4a59abeb7be58dd0c7d9a
lasender1 <lasender.ls@gmail.com>
2016-03-01 09:14:13 +0100

    Hasen geordnet+Nahrungskette

--- Hasilein^^ the original/src/hasilein/the/original/HasileinTheOriginal.java
+++ Hasilein^^ the original/src/hasilein/the/original/HasileinTheOriginal.java
@@ -55,25 +55,36 @@ public class HasileinTheOriginal {
             {
                meinArray[i][j]=  ".";    //die Tabelle geht nur von 0 bis 13 wenn man bei for int i/j=0 einsetzt 
                meinArray[1][1]=  "H";   //wenn man von 1 bis 14 haben will setzt man 1 dort ein,hatt dann aber nur 13 zeilen
-               meinArray[2][6]=  "H";
-               meinArray[13][3]= "H";
+               meinArray[10][10]="H";
+               meinArray[11][3]= "H";
                meinArray[3][6]=  "H";
                meinArray[12][8]= "H";
                meinArray[8][2]=  "H";
                meinArray[2][10]= "H";
                meinArray[5][1]=  "H";
                meinArray[13][5]= "H";
-               meinArray[2][7]=  "H";
-               meinArray[1][4]=  "H"; 
+               meinArray[6][5]=  "H";
+               meinArray[1][4]=  "H";
+               meinArray[0][6]=  "H";
+               meinArray[7][12]= "H";
                meinArray[4][7]=  "H";
                meinArray[12][8]= "H";
-               meinArray[13][6]= "H";
+               meinArray[10][6]= "H";
+               meinArray[12][12]="H";
+               meinArray[5][11]="H";
                meinArray[6][8]=  "H";
                meinArray[12][10]="F";
                meinArray[7][10]= "F";
                meinArray[3][0]=  "F";
                meinArray[5][13]= "F";
                meinArray[9][7]=  "F";
+               meinArray[5][8]=  "F";
+               meinArray[3][11]= "F";
+               meinArray[0][3]=  "F";
+               meinArray[8][4]=  "F";
+               meinArray[12][1]= "F";
+               meinArray[4][3]=  "F";
+               meinArray[1][12]= "F";
                System.out.print(meinArray[i][j]+" ");
             }
    
@@ -112,8 +123,14 @@ private static void moveHasen(){
                         meinArray[i][j]= ".";
                         meinArray[i][j+1]="H";
                     }
-                }
-        if (meinArray2[i][j].equals("F"))
+                }}}
+       
+          for (int i=0; i<meinArray2.length; i++)
+        {
+            for(int j=0; j<meinArray2.length; j++)
+            {     
+               
+               if (meinArray2[i][j].equals("F"))
                 {
                     
                     if(i==13)
@@ -127,9 +144,9 @@ private static void moveHasen(){
                         meinArray[i+1][j]="F";
                     }
                 }  
-            }
+            }}
             
-            
-}
+      
+
 }
 }
